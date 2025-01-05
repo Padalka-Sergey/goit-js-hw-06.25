8bdd8318e88feb9fb6272a6dc428b847160d2c69
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-05 15:22:57 +0200

    Task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const makeList = ingredients.map(el => {
+  const itemRef = document.createElement('li');
+  itemRef.textContent = el;
+  itemRef.classList.add('item');
+  return itemRef;
+});
+
+const listRef = document.querySelector('#ingredients');
+listRef.append(...makeList);
+
+// const makeList = items => {
+//   return items.map(element => {
+//     const itemRef = document.createElement('li');
+//     itemRef.textContent = element;
+//     itemRef.classList.add('item');
+//     return itemRef;
+//   });
+// };
+
+// const listRef = document.querySelector('#ingredients');
+// const arrRef = makeList(ingredients);
+// console.log(arrRef);
+// listRef.append(...arrRef);
