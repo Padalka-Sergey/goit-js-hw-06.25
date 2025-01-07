d540e51a3a2656e47a538749ea60195d29823c42
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-07 21:58:29 +0200

    Task 08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', handleSubmitForm);
+
+function handleSubmitForm(evt) {
+  evt.preventDefault();
+  const form = evt.target;
+  const email = form.elements.email;
+  const password = form.elements.password;
+  const obj = {};
+
+  if (email.value === '' || password.value === '') {
+    return alert('Все поля нужно заполнить!!!');
+  }
+
+  obj[email.name] = email.value;
+  obj[password.name] = password.value;
+  console.log(obj);
+
+  form.reset();
+}
