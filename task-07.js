f02458a9f6c88bf1e723baf5555b6b3fca3b8545
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-07 17:54:34 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-06.25@71bc309562dbe99db00fd7524c8e2c9ac9e91109 🚀

---
+++ task-07.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const n=document.querySelector("#font-size-control"),o=document.querySelector("#text");n.addEventListener("input",r);function r(e){const{value:t}=e.currentTarget;o.style.fontSize=`${t}px`}
+//# sourceMappingURL=task-07.js.map
