b38db01631d1c4893653262b3653d8d0f196b514
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-08 20:05:14 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-06.25@4fab0509edaeddff2bb8f611ec4e4a08f019b6f8 🚀

---
+++ task-09.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector("p .color"),e=document.querySelector("button.change-color"),n=document.querySelector("body");e.addEventListener("click",r);function r(){t.textContent=o(),n.style.backgroundColor=o()}function o(){return`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=task-09.js.map
