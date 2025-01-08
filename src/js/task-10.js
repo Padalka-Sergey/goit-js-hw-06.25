9fb6e0b3cefb1b4cb0f620d7295a6b13e0b6fd90
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-08 22:12:31 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,31 @@
+const inputRef = document.querySelector('input[type="number"]');
+const createBtnRef = document.querySelector('button[data-create]');
+const destroyBtnRef = document.querySelector('button[data-destroy]');
+const boxRef = document.querySelector('#boxes');
+
+let amount = null;
+inputRef.addEventListener('input', inputValueHandler);
+createBtnRef.addEventListener('click', createBoxes);
+destroyBtnRef.addEventListener('click', destroyHandler);
+
+function inputValueHandler() {
+  amount = Number(inputRef.value);
+}
+
+function createBoxes() {
+  destroyHandler();
+  for (let idx = 1; idx <= amount; idx += 1) {
+    const markup = `<div style="background-color: ${getRandomHexColor()}; width: ${
+      30 + idx * 10
+    }px; height: ${30 + idx * 10}px; margin-top: 10px"></div>`;
+    boxRef.insertAdjacentHTML('beforeend', markup);
+  }
+}
+
+function destroyHandler() {
+  boxRef.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
